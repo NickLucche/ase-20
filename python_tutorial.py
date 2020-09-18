4a2df96c00074332eddf61ce5c3f8b3421c64d5c
NickLucche <nicolo.lucchesi@gmail.com>
2020-09-18 10:55:09 +0200

    forgot the file lol

---
+++ python_tutorial.py
@@ -0,0 +1,33 @@
+#calculator.py
+
+def sum(m, n):
+    for _ in range(abs(n)):
+        m += (1 if n>0 else -1)
+    return m
+
+def divide(m, n):
+    counter = 0
+    n, m = abs(n), abs(m)
+    while m >= n:
+        m -= n
+        counter += 1
+
+    return -counter if m*n < 0 else counter 
+
+class FooCalculator:
+
+    def __init__(self) -> None:
+        pass
+    def sum(self, m, n):
+        sum(m, n)
+    def divide(self, m, n):
+        divide(m, n)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv)<3:
+        print("Usage python python_tutorial m n")
+        exit()
+    m, n = int(sys.argv[1]), int(sys.argv[2])
+    print("Sum:", sum(m, n))
+    print("Division:", divide(m, n))
